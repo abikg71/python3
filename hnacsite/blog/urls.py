88e01fa6053f4daa8698a969738bba0bfa6c4e4d
abikg71 <abinetkenore14@yahoo.com>
2020-02-07 17:52:53 -0700

    file path for static/blog cd ..

--- hnacsite/blog/urls.py
+++ hnacsite/blog/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('profile/', views.profile, name='blog-profile'),
-    path('profile/', views.profile, name='blog-profile'),
+    
 ]
