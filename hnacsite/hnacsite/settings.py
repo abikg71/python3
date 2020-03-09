25f5b02e4f21a41ef090981b8d0cb98d6bb5dafc
abikg71 <abinetkenore14@yahoo.com>
2020-03-08 22:53:09 -0600

    Upto 030820

--- hnacsite/hnacsite/settings.py
+++ hnacsite/hnacsite/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy.forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
+CRISPY_TEMPLATE_PACK = 'bootstarp4'
